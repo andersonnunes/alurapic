3a73911eca71b0afc3bd4b9948799b1ed169dce0
Anderson Nunes <anderson.nunes1988@gmail.com>
2017-09-02 19:41:43 -0300

    Aproveitando seu conhecimento de jQuery

--- client/app/listagem/listagem.component.ts
+++ client/app/listagem/listagem.component.ts
@@ -1,43 +1,51 @@
 import { Component } from '@angular/core';
-import { FotoService } from '../foto/foto.service';
+import { Http } from '@angular/http';
 import { FotoComponent } from '../foto/foto.component';
+import { FotoService } from '../foto/foto.service';
+import { PainelComponent } from '../painel/painel.component';
 
 @Component({
     moduleId: module.id,
     selector: 'listagem',
-    templateUrl: './listagem.component.html'
+    templateUrl: './listagem.component.html' 
 })
-export class ListagemComponent {
-    
+export class ListagemComponent { 
+
     fotos: FotoComponent[] = [];
     service: FotoService;
     mensagem: string = '';
 
     constructor(service: FotoService) {
-
         this.service = service;
-        this.service
-            .lista()
-            .subscribe(fotos => {
-                this.fotos = fotos;
-            }, erro => console.log(erro));
 
+        this.service.lista()
+            .subscribe(
+                fotos => this.fotos = fotos,
+                erro => console.log(erro)
+            );
     }
 
-    remove(foto: FotoComponent): void {
-        this.service.remove(foto)
-            .subscribe(
-                fotos => {
+    remove(foto: FotoComponent, painel: PainelComponent) {
 
-                    let novasFotos = this.fotos.slice(0);
-                    let indice = novasFotos.indexOf(foto);
-                    novasFotos.splice(indice, 1);
-                    this.fotos = novasFotos;
-                    this.mensagem = 'Foto removida com sucesso';
-                },
+        this.service
+            .remove(foto)
+            .subscribe(
+                () => {
+
+                    painel.fadeOut(() => {
+
+                        let novasFotos = this.fotos.slice(0);
+                        let indice = novasFotos.indexOf(foto);
+                        novasFotos.splice(indice, 1);
+                        this.fotos = novasFotos;
+                        this.mensagem = 'Foto removida com sucesso';
+                    }); 
+                }, 
                 erro => {
                     console.log(erro);
                     this.mensagem = 'Não foi possível remover a foto';
-                });
+                }
+            );
+
     }
 }
